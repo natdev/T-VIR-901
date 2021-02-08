47a89c5d921700c81d70decda615f42e23f208d8
natdev <dordonne.nathanael@gmail.com>
2021-02-08 18:23:26 +0100

    déplacement zombie et joueur

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+
+	public Wave[] waves;
+	public GameObject enemy;
+
+	Wave currentWave;
+	int currentWaveNumber;
+
+	int enemiesRemainingToSpawn;
+	int enemiesRemainingAlive;
+	float nextSpawnTime;
+
+	void Start()
+	{
+		NextWave();
+	}
+
+	void Update()
+	{
+
+		if (enemiesRemainingToSpawn > 0 && Time.time > nextSpawnTime)
+		{
+			enemiesRemainingToSpawn--;
+			nextSpawnTime = Time.time + currentWave.timeBetweenSpawns;
+
+			GameObject spawnedEnemy = Instantiate(enemy, Vector3.zero, Quaternion.identity) as GameObject;
+			
+		}
+	}
+
+	void OnEnemyDeath()
+	{
+		enemiesRemainingAlive--;
+
+		if (enemiesRemainingAlive == 0)
+		{
+			NextWave();
+		}
+	}
+
+	void NextWave()
+	{
+		currentWaveNumber++;
+		print("Wave: " + currentWaveNumber);
+		if (currentWaveNumber - 1 < waves.Length)
+		{
+			currentWave = waves[currentWaveNumber - 1];
+
+			enemiesRemainingToSpawn = currentWave.enemyCount;
+			enemiesRemainingAlive = enemiesRemainingToSpawn;
+		}
+	}
+
+	[System.Serializable]
+	public class Wave
+	{
+		public int enemyCount;
+		public float timeBetweenSpawns;
+	}
+
+}
